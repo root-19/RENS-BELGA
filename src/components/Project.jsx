5e1a969482a29514d97430e0cea59b421ecbe3b6
wasieDev <127023454+rens20@users.noreply.github.com>
2024-02-09 23:43:26 +0800

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,13 +9,46 @@ import Movie from "../assets/Movie.png";
 import Travel from "../assets/travel.png";
 const Projects = () =>{
     return(
-        <div>
-        <p className="text-2xl font-bold underline bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text mt-4">
-     Projects
-       
-      </p>
-      <div className="flex justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl">
+      <div>
+      <h2 className="text-2xl font-bold underline bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text">
+          Achievements
+      </h2>
+
+
+      <div className="relative">
+          <div className="absolute top-0 left-0 w-full overflow-hidden bg-gradient-to-r from-blue-500 via-cyan-400 to-blue-700">
+              <div className="flex animate-slide items-center">
+                  <a href="https://udemy-certificate.s3.amazonaws.com/image/UC-3e9e264c-cb78-477e-bdb0-712ea3e84ab4.jpg" target="_blank" rel="noopener noreferrer">
+                      <img
+                          src="https://udemy-certificate.s3.amazonaws.com/image/UC-3e9e264c-cb78-477e-bdb0-712ea3e84ab4.jpg"
+                          alt="Image 1"
+                          className="h-auto w-auto max-w-full max-h-40 mr-10 "
+                      />
+                  </a>
+                  <a href="https://udemy-certificate.s3.amazonaws.com/image/UC-3e9e264c-cb78-477e-bdb0-712ea3e84ab4.jpg" target="_blank" rel="noopener noreferrer">
+                      <img
+                          src="https://udemy-certificate.s3.amazonaws.com/image/UC-3e9e264c-cb78-477e-bdb0-712ea3e84ab4.jpg"
+                          alt="Image 1"
+                          className="h-auto w-auto max-w-full max-h-40 mr-10 "
+                      />
+                  </a>
+                  <a href="https://udemy-certificate.s3.amazonaws.com/image/UC-3e9e264c-cb78-477e-bdb0-712ea3e84ab4.jpg" target="_blank" rel="noopener noreferrer">
+                      <img
+                          src="https://udemy-certificate.s3.amazonaws.com/image/UC-3e9e264c-cb78-477e-bdb0-712ea3e84ab4.jpg"
+                          alt="Image 1"
+                          className="h-auto w-auto max-w-full max-h-40 mr-10"
+                      />
+                  </a>
+              </div>
+          </div>
+  
+    
+          <p className="text-2xl md:text-3xl lg:text-4xl font-bold pt-20 md:pt-30 lg:pt-40 underline bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text mt-4">
+           Projects
+          </p>
+
+      <div className="flex justify-center ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl pt-5">
         {/* Blog Post 1 */}
         <div className="border rounded overflow-hidden">
           <img
@@ -27,7 +60,9 @@ const Projects = () =>{
             <h4 className="text-xl font-bold mb-2">
               <a href="https://maria-ai-two.vercel.app/" className="bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text" >Maria Ai</a>
             </h4>
-            <p className="text-gray-700">I created Maria Ai using React.js, Tailwind CSS, DaisyUI, and Node.js. This AI has its own perspective; for instance, if you talk, the response is tailored to what you said. </p>
+            <p className="text-gray-700">
+             I created Maria Ai using react.js,tailwind,daisyUi and node.js.
+            </p>
           </div>
         </div>
 
@@ -40,9 +75,10 @@ const Projects = () =>{
           />
           <div className="p-4">
             <h4 className="text-xl font-bold mb-2">
-            <a href="https://expense-tracker-fawn-eight.vercel.app/"  className="bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text">Expenses tracker</a>
+            <a href="https://expense-tracker-fawn-eight.vercel.app/"  className="bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text">Expnses tracker</a>
             </h4>
-            <p className="text-gray-700">I built an Expenses Tracker using React.js and Tailwind CSS with DaisyUI. I built this because of my experimental teacher.
+            <p className="text-gray-700">
+             i builed EXpenses tracker  using react.js and tailwind with daisyUi i builed this beacuse of my expoerwment teacher
             </p>
           </div>
         </div>
@@ -76,7 +112,7 @@ const Projects = () =>{
               <a href="https://recipe-tau-taupe.vercel.app/"  className="bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text">Recipe List</a>
             </h4>
             <p className="text-gray-700">
-             I created a Recipe List using React.js and Tailwind CSS. This is my project to enhance my knowledge. I created this to practice how to handle React queries.
+             Recipe list I create using react.js and tailwind this my project to enchance my knowledge i create this to practice how to handle React query
             </p>
           </div>
         </div>
@@ -92,7 +128,7 @@ const Projects = () =>{
               <a href="https://cat-chat-six.vercel.app/" className="bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text">Cat Chat</a>
             </h4>
             <p className="text-gray-700">
-             Cat Chat incorporates Google account authentication for user login, providing a convenient and secure way for users to access the platform. By integrating Google account authentication, users can seamlessly log in using their existing Google credentials, enhancing the user experience and ensuring authentication security.
+             Cat chat is a chat application using react.js and firebase when I builed have a lot knowledge having I build.
             </p>
           </div>
         </div>
@@ -108,7 +144,7 @@ const Projects = () =>{
               <a href="https://videocall-eight.vercel.app/" className="bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text">VideoCall</a>
             </h4>
             <p className="text-gray-700">
-             Video Call is a one-on-one video call application built using HTML, CSS, and JavaScript. I developed this project to improve my skills in web development and to provide a platform for seamless video communication.
+             video call is one on one Videocall using html,css and javascirpt 
             </p>
           </div>
         </div>
@@ -124,7 +160,7 @@ const Projects = () =>{
               <a href="https://e-book-omega.vercel.app/"  className="bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text">Movie list</a>
             </h4>
             <p className="text-gray-700">
-             The Movie List project is built with HTML, CSS, and JavaScript, featuring a collection of movie trailers and occasionally full-length films. This application aims to provide users with a curated selection of movie trailers and full videos for entertainment purposes. Developed to sharpen my front-end development skills, this project showcases my ability to create visually appealing and interactive web applications.
+             Movie lis is create using html css and js this movie is trailer movies but somethis is full video
             </p>
           </div>
         </div>
@@ -140,11 +176,12 @@ const Projects = () =>{
               <a href="https://tourtravel-two.vercel.app/"  className="bg-gradient-to-r text-transparent p-2 from-blue-500 via-cyan-400 to-blue-700 bg-clip-text">Travel tour</a>
             </h4>
             <p className="text-gray-700">
-The Travel Tour webpage is a project I created for my client using React.js and Tailwind CSS. This website is designed to showcase various travel destinations and tour packages offered by my client's business. By utilizing React.js and Tailwind CSS, I aimed to deliver a modern and responsive user experience while demonstrating my expertise in front-end development to meet my client's needs effectively.
+             travel tour is webpage i create to my clinent using react.js and tailwind
             </p>
           </div>
         </div>
       </div>
+    </div>
     </div>
 
         </div>
