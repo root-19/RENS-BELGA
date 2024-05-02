b9885e5fc181d8435c07f89a35b065d6649760c0
Rens  Acuña <127023454+rens20@users.noreply.github.com>
2024-05-03 03:32:27 +0800

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -43,8 +43,8 @@ const Projects = () =>{
           </div>
   
     
-                <h2 className="text-2xl font-bold ml-6   text-slate-950 bg-clip-text">
-Project</h2>
+                <p className="text-2xl font-bold ml-6   text-slate-950 bg-clip-text">
+Project</p>
 <div className="flex justify-center">
   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl pt-5">
     {/* Blog Post 1 */}
