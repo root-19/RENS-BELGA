49178e3c136591982bfa916c869667f846732f4c
Rens  Acuña <127023454+rens20@users.noreply.github.com>
2024-05-03 20:44:01 +0800

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,6 +13,7 @@ const Projects = () =>{
       <h2 className="text-2xl font-bold ml-6   text-slate-950 bg-clip-text">
           Achievements
       </h2>
+          <br></br>
 
 
       <div className="relative">
@@ -43,7 +44,7 @@ const Projects = () =>{
           </div>
   
     
-                <p className="text-2xl font-bold ml-6   text-slate-950 bg-clip-text">
+                <p className=" text-2xl font-bold ml-6   text-slate-950 bg-clip-text">
 Project</p>
 <div className="flex justify-center">
   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl pt-5">
